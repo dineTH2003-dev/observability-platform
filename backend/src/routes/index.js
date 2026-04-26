2de1bdf2746948d6af9b7ccf2af9d4aa3e317854
muthu-24 <bhagyamadhumi308@gmail.com>
2026-04-26 15:50:41 +0530

    add report route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,5 +6,6 @@ router.use("/hosts", require("./host.routes"));
 router.use("/auth", require("./auth.routes"));
 router.use("/services", require("./service.routes"));
 router.use("/agent", require("./agent.routes"));
+router.use("/reports", require("./reports.routes"));
 
 module.exports = router;

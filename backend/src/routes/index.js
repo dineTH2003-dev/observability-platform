a0df49e65ea3b13adb8e5c13110607a8b729e21c
muthu-24 <bhagyamadhumi308@gmail.com>
2026-04-27 03:31:39 +0530

    add alert path

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,5 +8,6 @@ router.use("/services", require("./service.routes"));
 router.use("/agent", require("./agent.routes"));
 router.use("/reports", require("./reports.routes"));
 router.use("/incidents", require("./incident.routes"));
+router.use("/alerts",require("./alert.routes"));
 
 module.exports = router;

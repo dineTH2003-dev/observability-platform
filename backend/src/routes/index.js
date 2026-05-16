3ab127a3b3748a7769ecb9a030ad5048f5cec414
muthu-24 <bhagyamadhumi308@gmail.com>
2026-05-16 16:38:44 +0530

    update routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/test", (req, res) => {
+  res.json({
+    success: true,
+    message: "Backend is working"
+  });
+});
+
+
 router.use("/applications", require("./application.routes"));
 router.use("/hosts", require("./host.routes"));
 router.use("/auth", require("./auth.routes"));
 router.use("/services", require("./service.routes"));
 router.use("/agent", require("./agent.routes"));
-router.use("/reports", require("./reports.routes"));
+
+router.use("/reports", require("./report.routes"));
+
 router.use("/incidents", require("./incident.routes"));
-router.use("/alerts",require("./alert.routes"));
-router.use("/alert-settings",require("./alertSettings.routes"))
+router.use("/alerts", require("./alert.routes"));
+router.use("/alert-settings", require("./alertSettings.routes"));
 
 module.exports = router;
